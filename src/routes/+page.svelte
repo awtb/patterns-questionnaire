aa538041e0b0745515c34f6be86753a116a14af7
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-03-22 14:47:37 +0500

    feat(routes): render questionnaire preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Hello world</h1>
+<script lang="ts">
+  import QuestionCard from '$lib/components/QuestionCard.svelte';
+  import { questions } from '$lib/data/questions';
+
+  const currentQuestion = questions[0];
+</script>
+
+<QuestionCard question={currentQuestion} current={1} total={questions.length} />
