31b5df703373aa6d724853a27f607a4315bb5a5a
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-03-22 16:36:06 +0500

    feat(routes): add questionnaire page

---
+++ src/routes/questionnaire/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Questionnaire from '$lib/components/Questionnaire.svelte';
+  import { questions } from '$lib/data/questions';
+</script>
+
+<Questionnaire {questions} />
