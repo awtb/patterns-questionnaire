2567db6325425dffac47ed90acd819bfc5ceea8f
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-03-22 16:49:09 +0500

    chore(config): update svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from "@sveltejs/adapter-static"
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-export default {
+const dev = process.argv.includes("dev");
+
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       fallback: "200.html"
-    })
+    }),
+    paths: {
+      base: dev ? "" : "/patterns-questionnaire"
+    }
   }
-}
+};
+
+export default config;
